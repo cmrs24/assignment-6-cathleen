486e038c4ad3ba7b423f67b17ac405430c68f80c
cmrs24 <cathleen2479@gmail.com>
2017-05-11 20:34:01 -0400

    Assignment-6-cathleen

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -1,67 +1,41 @@
-/*
-//city-type should be named city-name
-//make it switch once and then go from there
-//style.css file has classes for images... so when someone types in name of city, apply a class
-//the way to apply the class is .att() >> $('body').att set body equal to class
-//('#submit-btn').click(selectCity);
-*/
 
-/*wait to run jquery*/
+/*Cathleen's note: I did not have much on my own for assignment 6, I had a very difficult 
+time with this assignment! Will be re-doing over the weekend */
+
+
+var cities = ["NYC", "SF", "LA", "ATX", "SYD"];
 
 	$(document).ready(function(){
 
-/*user enters city name and clicks update*/
 
-	$("#submit-btn").click(updateBackground);
-
-/*create function*/
-/*prevent form submission*/
-	function updateBackground(event) {
-		event.preventDefault();
-	
-
-/*capture & store the value*/
-
-	var cityChoice = $("#city-type").val();
-
-/*change background css class depending on value*/
-
-	if (cityChoice === 'New York' || cityChoice === 'New York City'|| cityChoice === 'NYC' || cityChoice === 'new york' || cityChoice === 'new york city' || cityChoice === 'nyc') {
-
-	$('body').removeClass('background').addClass('nyc');
-
+	for(i=0;i<cities.length;i++) {
+		$('#city-type').append('<option value="' + cities[i] + '">' + cities[i] + '</option>');
 	}
 
-	else if (cityChoice === 'San Francisco' || cityChoice === 'SF'|| cityChoice === 'Bay Area') {
 
-	$('body').removeClass('background').addClass('sf');
-
+$('form').on('change', '#city-type', function(){
+	var city = $('#city-type').val();
+	if (city === 'NYC') {
+		$('body'.attr('class', 'nyc'); 
 	}
 
-	else if (cityChoice === 'Los Angeles' || cityChoice === 'LA'|| cityChoice === 'LAX') {
+	else if (city === 'SF') {
+	$('body'.attr('class', 'sf');
+	}	
 
-	$('body').removeClass('background').addClass('la');
 
+	else if (city === 'LA') {
+	$('body'.attr('class', 'la');
 	}
 
-	else if (cityChoice === 'Austin' || cityChoice === 'ATX') {
-
-	$('body').removeClass('background').addClass('austin');
-
+	else if (city === 'LA') {
+	$('body'.attr('class', 'la');
 	}
+
+
+});
+
+
   
-	else if (cityChoice === 'Sydney' || cityChoice === 'SYD') {
-
-	$('body').removeClass('background').addClass('sydney');
-
-	}
-
-	else {
-
-	$('body').addClass('background');
-
-	}
-  
-}
 
 });
